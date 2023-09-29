c74fc9dee9d51600abbedd857b56bd69f977737c
mehran sasaninia <111546668+smilemehran@users.noreply.github.com>
2023-09-29 09:48:55 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,30 @@
 import socket
 
+# Create a UDP socket
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server.bind(("127.0.0.1",7777))
+server.bind(("127.0.0.1", 7777))
 
-fileName,addr = server.recvfrom(1024)
+fileName, addr = server.recvfrom(1024)
 print(fileName.decode('utf-8'))
 fileSize = server.recvfrom(1024)[0].decode('utf-8')
 print(fileSize)
 
+# Open a file to write the received data
 file = open(fileName, "wb")
-fileBytes = b""
+total_data = 0
 
-for i in range(0,30):
-    data = server.recv(1024)
-    fileBytes += data
+while total_data < int(fileSize):
+    # Receive data from the client
+    data, addr = server.recvfrom(1024)
+    if not data:
+        break
 
-file.write(fileBytes)
+    # Write the received data to the file
+    file.write(data)
+    total_data += len(data)
 
-server.close()
+    # Send an acknowledgment back to the client
+    server.sendto(b"ACK", addr)
+
+file.close()
+print(f"File received from {addr}")
